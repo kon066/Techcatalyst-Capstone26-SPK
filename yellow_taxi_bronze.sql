8e9c4bd4f7d049bb46918dfb639dc1965da3a3c6
Paul Doherty <pauldoherty2004@gmail.com>
2026-08-11 17:52:07 +0000

    updated bronze sql queries

--- yellow_taxi_bronze.sql
+++ yellow_taxi_bronze.sql
@@ -1,10 +1,5 @@
-USE ROLE DE;
-USE WAREHOUSE COMPUTE_WH;
-USE DATABASE TECHCATALYST;
-USE SCHEMA TECHCATALYST.GROUP_SPK;
-
 SELECT *
-FROM yellow_taxi_standardized
+FROM yellow_taxi_raw
 WHERE
     fare_amount >= 0
     AND total_amount >= 0
@@ -12,11 +7,11 @@ WHERE
     AND passenger_count > 0
     AND trip_distance > 0
     AND trip_distance <= 200
-    AND dropoff_datetime > pickup_datetime
+    AND tpep_dropoff_datetime > tpep_pickup_datetime
     AND DATEDIFF(
             minute,
-            pickup_datetime,
-            dropoff_datetime
+            tpep_pickup_datetime,
+            tpep_dropoff_datetime
         ) BETWEEN 1 AND 240
     AND NOT (
         trip_distance = 0
